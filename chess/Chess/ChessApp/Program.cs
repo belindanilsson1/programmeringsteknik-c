adb67fdcc2a968fdfa617bc99e5a271eff7173d0
LAPTOP-JCGE0C4K\Lenovo <nilssonbelinda@outlook.com>
2020-08-28 15:20:37 +0200

    ChessApp

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.Write("▓");
+           // Console.Write("░");
+           // Console.ReadKey();
+
+
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 16; x++)
+                {
+                    var characterIndex = ((x / 2) + y) % 2;
+                    var character = characterIndex == 0 ? '░' : '▓';
+
+                    Console.Write(character);
+                }
+
+                Console.Write('\n');
+            }
+
+            Console.ReadKey();
+
+            {
+               
+            }
+        }
+    }
+}
